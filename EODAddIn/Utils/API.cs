21412b9080d6e91170d0e45a88136538dd991a40
Zheltov Alexey <zheltiy86@gmail.com>
2021-07-19 21:48:13 +0300

    Утилитный класс для отправки POST GET запросов

---
+++ EODAddIn/Utils/API.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using System.Text;
+
+namespace EODAddIn.Utils
+{
+    /// <summary>
+    /// Класс для отправки и получения запросов от сторонних сервисов
+    /// </summary>
+    public static class API
+    {
+        /// <summary>
+        /// Отправка POST запроса
+        /// </summary>
+        /// <param name="Url">Адрес</param>
+        /// <param name="Data">Параметры запроса</param>
+        /// <returns></returns>
+        public static string POST(string Url, string Data)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            WebRequest req = WebRequest.Create(Url);
+            req.Method = "POST";
+            req.Timeout = 100000;
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            byte[] sentData = Encoding.GetEncoding("UTF-8").GetBytes(Data);
+            req.ContentLength = sentData.Length;
+            System.IO.Stream sendStream = req.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
+            WebResponse res = req.GetResponse();
+            System.IO.Stream ReceiveStream = res.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8);
+            //Кодировка указывается в зависимости от кодировки ответа сервера
+            char[] read = new char[256];
+            int count = sr.Read(read, 0, 256);
+            string Out = string.Empty;
+            while (count > 0)
+            {
+                string str = new string(read, 0, count);
+                Out += str;
+                count = sr.Read(read, 0, 256);
+            }
+            return Out;
+        }
+
+        /// <summary>
+        /// GET запрос к ресурсу
+        /// </summary>
+        /// <param name="Url">Адрес ресурса</param>
+        /// <param name="Data">Данные</param>
+        /// <returns></returns>
+        public static string GET(string Url, string Data = "")
+        {
+            byte[] qwe;
+            if (Data == "")
+            {
+                qwe = Encoding.Unicode.GetBytes(Url);
+            }
+            else
+            {
+                qwe = Encoding.Unicode.GetBytes(Url + "?" + Data);
+            }
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            WebRequest req = WebRequest.Create(Encoding.Unicode.GetString(qwe));
+            WebResponse resp = req.GetResponse();
+            System.IO.Stream stream = resp.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            string Out = sr.ReadToEnd();
+            sr.Close();
+
+            return Out;
+        }
+    }
+}
