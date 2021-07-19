d2a0cff409789f0fb767ec5df028caadd610a821
Zheltov Alexey <zheltiy86@gmail.com>
2021-07-19 21:47:00 +0300

    Сохранение пользовательских настроек

---
+++ EODAddIn/Program/Settings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EODAddIn.Program
+{
+    internal static class Settings
+    {
+        private static readonly string xmlFilename = "settings.xml";
+        private static readonly string path;
+        internal static SettingsFields SettingsFields;
+
+        static Settings()
+        {
+            SettingsFields = new SettingsFields();
+            path = Path.Combine(Program.UserFolder, xmlFilename);
+            Read();
+        }
+
+        /// <summary>
+        /// Чтение настроек
+        /// </summary>
+        internal static void Read()
+        {
+            if (!File.Exists(path)) Save();
+
+            try
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(SettingsFields));
+                using (FileStream fs = File.OpenRead(path))
+                {
+                    SettingsFields = (SettingsFields)formatter.Deserialize(fs);
+                }
+            }
+            catch (Exception)
+            {
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// Сохранение настроек
+        /// </summary>
+        internal static void Save()
+        {
+            try
+            {
+                if (!Directory.Exists(Program.UserFolder)) Directory.CreateDirectory(Program.UserFolder);
+                XmlSerializer formatter = new XmlSerializer(typeof(SettingsFields));
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    formatter.Serialize(fs, SettingsFields);
+                }
+            }
+            catch (Exception ex)
+            {
+                new ErrorReport(ex).Send();
+            }
+        }
+    }
+}
---
+++ EODAddIn/Program/SettingsFields.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EODAddIn.Program
+{
+    [Serializable]
+    public class SettingsFields
+    {
+        /// <summary>
+        /// API ключ
+        /// </summary>
+        public string APIKey = string.Empty;
+
+
+        public SettingsFields()
+        {
+
+        }
+    }
+}
