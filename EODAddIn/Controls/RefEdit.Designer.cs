be27bc9e3f3809f23f3e26c0f7b1b6301b3ca70f
Zheltov Alexey <zheltiy86@gmail.com>
2021-10-25 11:28:14 +0300

    Добавление контрола RefEdit для выделения диапазонов из Excel

---
+++ EODAddIn/Controls/RefEdit.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace ProXL.Forms
+{
+    partial class RefEdit
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RefEdit));
+            this.txtRef = new System.Windows.Forms.TextBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRef
+            // 
+            this.txtRef.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRef.Location = new System.Drawing.Point(0, 0);
+            this.txtRef.Multiline = true;
+            this.txtRef.Name = "txtRef";
+            this.txtRef.Size = new System.Drawing.Size(273, 20);
+            this.txtRef.TabIndex = 0;
+            this.txtRef.TextChanged += new System.EventHandler(this.txtRef_TextChanged);
+            this.txtRef.Enter += new System.EventHandler(this.TxtRef_Enter);
+            this.txtRef.Leave += new System.EventHandler(this.TxtRef_Leave);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUp.Image = ((System.Drawing.Image)(resources.GetObject("btnUp.Image")));
+            this.btnUp.Location = new System.Drawing.Point(273, -1);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(46, 22);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.ResizeForm);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDown.Image = ((System.Drawing.Image)(resources.GetObject("btnDown.Image")));
+            this.btnDown.Location = new System.Drawing.Point(273, -1);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(46, 22);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Visible = false;
+            this.btnDown.Click += new System.EventHandler(this.ResizeForm);
+            // 
+            // RefEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.txtRef);
+            this.Name = "RefEdit";
+            this.Size = new System.Drawing.Size(318, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRef;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+    }
+}
