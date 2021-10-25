d4198409ba8485a7c510ff113046a9f24cf45d89
Zheltov Alexey <zheltiy86@gmail.com>
2021-10-25 11:25:19 +0300

    Добавление класса WinHwnd для запуска окон от приложения Excel

---
+++ EODAddIn/Utils/WinHwnd.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EODAddIn.Utils
+{
+    public class WinHwnd : IWin32Window
+    {
+        public IntPtr Handle => (IntPtr)Globals.ThisAddIn.Application.ActiveWindow.Hwnd;
+    }
+}
