05a3e6aeea2c66d1d663561d15c801000893cf8f
Виталий Комышев <vkomyshev@micro-solution.ru>
2024-03-26 16:56:12 +0300

    [fix] different settings panels for different windows

--- EODAddIn/Forms/FrmGetBulkEod.designer.cs
+++ EODAddIn/Forms/FrmGetBulkEod.designer.cs
@@ -42,6 +42,8 @@
             this.BtnGet = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tbExchange = new System.Windows.Forms.TextBox();
+            this.RadioExchange = new System.Windows.Forms.RadioButton();
+            this.RadioTickers = new System.Windows.Forms.RadioButton();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridTickers)).BeginInit();
             this.SuspendLayout();
@@ -54,7 +56,7 @@
             this.tsmiClearTicker});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(284, 25);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,7 +64,7 @@
             // 
             this.tsmiFindTicker.Image = ((System.Drawing.Image)(resources.GetObject("tsmiFindTicker.Image")));
             this.tsmiFindTicker.Name = "tsmiFindTicker";
-            this.tsmiFindTicker.Size = new System.Drawing.Size(90, 20);
+            this.tsmiFindTicker.Size = new System.Drawing.Size(95, 21);
             this.tsmiFindTicker.Text = "Find ticker";
             this.tsmiFindTicker.Click += new System.EventHandler(this.tsmiFindTicker_Click);
             // 
@@ -73,14 +75,14 @@
             this.tsmiFromExcel});
             this.tsmiLoadTickers.Image = ((System.Drawing.Image)(resources.GetObject("tsmiLoadTickers.Image")));
             this.tsmiLoadTickers.Name = "tsmiLoadTickers";
-            this.tsmiLoadTickers.Size = new System.Drawing.Size(71, 20);
+            this.tsmiLoadTickers.Size = new System.Drawing.Size(75, 21);
             this.tsmiLoadTickers.Text = "Import";
             // 
             // tsmiFromTxt
             // 
             this.tsmiFromTxt.Image = ((System.Drawing.Image)(resources.GetObject("tsmiFromTxt.Image")));
             this.tsmiFromTxt.Name = "tsmiFromTxt";
-            this.tsmiFromTxt.Size = new System.Drawing.Size(165, 22);
+            this.tsmiFromTxt.Size = new System.Drawing.Size(177, 22);
             this.tsmiFromTxt.Text = "From file txt";
             this.tsmiFromTxt.Click += new System.EventHandler(this.tsmiFromTxt_Click);
             // 
@@ -88,7 +90,7 @@
             // 
             this.tsmiFromExcel.Image = ((System.Drawing.Image)(resources.GetObject("tsmiFromExcel.Image")));
             this.tsmiFromExcel.Name = "tsmiFromExcel";
-            this.tsmiFromExcel.Size = new System.Drawing.Size(165, 22);
+            this.tsmiFromExcel.Size = new System.Drawing.Size(177, 22);
             this.tsmiFromExcel.Text = "From Excel range";
             this.tsmiFromExcel.Click += new System.EventHandler(this.tsmiFromExcel_Click);
             // 
@@ -96,7 +98,7 @@
             // 
             this.tsmiClearTicker.Image = ((System.Drawing.Image)(resources.GetObject("tsmiClearTicker.Image")));
             this.tsmiClearTicker.Name = "tsmiClearTicker";
-            this.tsmiClearTicker.Size = new System.Drawing.Size(80, 20);
+            this.tsmiClearTicker.Size = new System.Drawing.Size(86, 21);
             this.tsmiClearTicker.Text = "Clear list";
             this.tsmiClearTicker.Click += new System.EventHandler(this.tsmiClearTicker_Click);
             // 
@@ -127,12 +129,12 @@
             this.gridTickers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridTickers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1});
-            this.gridTickers.Location = new System.Drawing.Point(12, 27);
+            this.gridTickers.Location = new System.Drawing.Point(12, 52);
             this.gridTickers.Name = "gridTickers";
             this.gridTickers.RowHeadersVisible = false;
             this.gridTickers.RowHeadersWidth = 20;
             this.gridTickers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gridTickers.Size = new System.Drawing.Size(260, 209);
+            this.gridTickers.Size = new System.Drawing.Size(260, 186);
             this.gridTickers.TabIndex = 12;
             // 
             // Column1
@@ -167,11 +169,36 @@
             this.tbExchange.Size = new System.Drawing.Size(164, 20);
             this.tbExchange.TabIndex = 15;
             // 
+            // RadioExchange
+            // 
+            this.RadioExchange.AutoSize = true;
+            this.RadioExchange.Location = new System.Drawing.Point(13, 29);
+            this.RadioExchange.Name = "RadioExchange";
+            this.RadioExchange.Size = new System.Drawing.Size(103, 17);
+            this.RadioExchange.TabIndex = 16;
+            this.RadioExchange.TabStop = true;
+            this.RadioExchange.Text = "Entire Exchange";
+            this.RadioExchange.UseVisualStyleBackColor = true;
+            this.RadioExchange.CheckedChanged += new System.EventHandler(this.RadioExchange_CheckedChanged);
+            // 
+            // RadioTickers
+            // 
+            this.RadioTickers.AutoSize = true;
+            this.RadioTickers.Location = new System.Drawing.Point(123, 29);
+            this.RadioTickers.Name = "RadioTickers";
+            this.RadioTickers.Size = new System.Drawing.Size(105, 17);
+            this.RadioTickers.TabIndex = 17;
+            this.RadioTickers.TabStop = true;
+            this.RadioTickers.Text = "Selected Tickers";
+            this.RadioTickers.UseVisualStyleBackColor = true;
+            // 
             // FrmGetBulkEod
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 345);
+            this.Controls.Add(this.RadioTickers);
+            this.Controls.Add(this.RadioExchange);
             this.Controls.Add(this.tbExchange);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.BtnGet);
@@ -207,5 +234,7 @@
         private System.Windows.Forms.Button BtnGet;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tbExchange;
+        private System.Windows.Forms.RadioButton RadioExchange;
+        private System.Windows.Forms.RadioButton RadioTickers;
     }
 }
